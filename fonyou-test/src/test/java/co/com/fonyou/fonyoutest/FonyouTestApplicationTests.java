61038081c7fed142b368c42b39bdc1a10d19f1a3
bonillasan <sbonilla0197@gmail.com>
2022-09-06 14:38:42 -0500

    carga del proyecto

---
+++ fonyou-test/src/test/java/co/com/fonyou/fonyoutest/FonyouTestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.fonyou.fonyoutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonyouTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
